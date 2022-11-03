33ba790af2a2ec754c48a18d44eb160910c55d8a
semissatto2 <semissatto@gmail.com>
2022-11-03 13:25:54 -0300

    Adiciona glue crawler

--- infraestructure/iam.tf
+++ infraestructure/iam.tf
@@ -75,3 +75,22 @@ resource "aws_iam_role_policy_attachment" "lambda_attach" {
   role       = aws_iam_role.lambda.name
   policy_arn = aws_iam_policy.lambda.arn
 }
+
+resource "aws_iam_role" "glue_role" {
+  name               = "semissatto_Role_GlueCrawler_rais"
+  path               = "/"
+  description        = "Provides write permissions to CloudWatch Logs and S3 Full Access"
+  assume_role_policy = file("./permissions/Role_GlueCrawler.json")
+}
+
+resource "aws_iam_policy" "glue_policy" {
+  name        = "semissatto_Policy_GlueCrawler_rais"
+  path        = "/"
+  description = "Provides write permissions to CloudWatch Logs and S3 Full Access"
+  policy      = file("./permissions/Policy_GlueCrawler.json")
+}
+
+resource "aws_iam_role_policy_attachment" "glue_attach" {
+  role       = aws_iam_role.glue_role.name
+  policy_arn = aws_iam_policy.glue_policy.arn
+}
