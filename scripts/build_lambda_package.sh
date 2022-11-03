8742cd23c9936d6856975555435ba2ae82591d42
semissatto2 <semissatto@gmail.com>
2022-11-02 22:23:05 -0300

    Modifica script

--- scripts/build_lambda_package.sh
+++ scripts/build_lambda_package.sh
@@ -45,6 +45,10 @@ then
 	echo "Compactando arquivo lambda_function_payload.zip"
 	zip -r9 ../lambda_function_payload.zip . #Compacta o pacote para o deploy
 	echo "Arquivo compactado com sucesso!"
+	echo "Diretorio atual"
+	echo pwd
+	cd ..
+	ls
 	echo "============================================="
 fi
 
